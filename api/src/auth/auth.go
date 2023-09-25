49f61b110b6d33aa80e71468b6b5713a75303c13
Liljack118 <kamilzajaczek4@gmail.com>
2023-09-25 20:56:57 +0200

    implement create token functions

--- api/src/auth/auth.go
+++ api/src/auth/auth.go
@@ -3,12 +3,17 @@ package auth
 import (
 	"forum/api/src/models"
 	"net/http"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 )
 
 type AuthJWT struct {
-	Request *http.Request
+	Request     *http.Request
+	secret      []byte `default:"supersecrethere"`
+	access_exp  int    `default:"900"`
+	refresh_exp int    `default:"43200"`
 }
 
 func (a *AuthJWT) getUserID() (uuid.UUID, error) {
@@ -38,10 +43,50 @@ func (a *AuthJWT) VerifyRefreshToken() bool {
 	return true
 }
 
-func (a *AuthJWT) CreateAccessToken(id uuid.UUID) string {
-	return "access_token"
+func (a *AuthJWT) createToken(claims *jwt.MapClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(a.secret)
+
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
 }
 
-func (a *AuthJWT) CreateRefreshToken(id uuid.UUID) string {
-	return "refresh_token"
+func (a *AuthJWT) CreateAccessToken(sub uuid.UUID) (string, error) {
+	claims := jwt.MapClaims{
+		"type": "access",
+		"sub":  sub,
+		"iat":  jwt.NewNumericDate(time.Now()),
+		"exp":  jwt.NewNumericDate(time.Now().Add(time.Second * time.Duration(a.access_exp))),
+	}
+
+	return a.createToken(&claims)
+}
+
+func (a *AuthJWT) CreateRefreshToken(sub uuid.UUID) (string, error) {
+	claims := jwt.MapClaims{
+		"type": "refresh",
+		"sub":  sub,
+		"iat":  jwt.NewNumericDate(time.Now()),
+		"exp":  jwt.NewNumericDate(time.Now().Add(time.Second * time.Duration(a.refresh_exp))),
+	}
+
+	return a.createToken(&claims)
+}
+
+func (a *AuthJWT) CreateTokens(sub uuid.UUID) (string, string, error) {
+	access_token, err := a.CreateAccessToken(sub)
+	if err != nil {
+		return "", "", err
+	}
+
+	refresh_token, err := a.CreateRefreshToken(sub)
+	if err != nil {
+		return "", "", err
+	}
+
+	return access_token, refresh_token, nil
 }
