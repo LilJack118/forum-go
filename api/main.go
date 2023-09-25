a452e789514960c8829e29b806a798c66d36d76b
Liljack118 <kamilzajaczek4@gmail.com>
2023-09-25 19:38:13 +0200

    add default middleware

--- api/main.go
+++ api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"forum/api/src/handlers"
+	"forum/api/src/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,6 +11,7 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router = router.PathPrefix("/api").Subrouter()
+	router.Use(middlewares.DefaultMiddleware)
 
 	// auth endpoints
 	router.HandleFunc("/register", handlers.RegisterHandler).Methods("POST")
