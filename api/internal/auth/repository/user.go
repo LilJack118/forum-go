5269f9867c1faf16c8ac7670484e00ed82cb4e9d
Liljack118 <kamilzajaczek4@gmail.com>
2023-09-30 20:46:52 +0200

    add createUser function

--- api/internal/auth/repository/user.go
+++ api/internal/auth/repository/user.go
@@ -1,8 +1,11 @@
 package repository
 
 import (
+	"context"
 	"forum/api/internal/models"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -14,7 +17,24 @@ func NewAuthRepository(db *mongo.Database) *authRepository {
 	return &authRepository{db}
 }
 
+func (repo *authRepository) getCollection() *mongo.Collection {
+	return repo.db.Collection("users")
+}
+
 func (repo *authRepository) CreateUser(user *models.User) error {
+	collection := repo.getCollection()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	buser, err := bson.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	if _, err := collection.InsertOne(ctx, buser); err != nil {
+		return err
+	}
 
 	return nil
 }
--- api/internal/models/user.go
+++ api/internal/models/user.go
@@ -9,11 +9,11 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `json:"user_id" db:"user_id" validate:"omitempty"`
-	FirstName string    `json:"first_name" db:"first_name" validate:"required,alphanum,lte=30"`
-	LastName  string    `json:"last_name" db:"last_name" validate:"required,alphanum,lte=30"`
-	Email     string    `json:"email" db:"email" validate:"omitempty,lte=60,email"`
-	Password  string    `json:"password" db:"password" validate:"omitempty,required,gte=6"`
+	ID        uuid.UUID `json:"user_id" bson:"user_id" validate:"omitempty"`
+	FirstName string    `json:"first_name" bson:"first_name" validate:"required,alphanum,lte=30"`
+	LastName  string    `json:"last_name" bson:"last_name" validate:"required,alphanum,lte=30"`
+	Email     string    `json:"email" bson:"email" validate:"omitempty,lte=60,email"`
+	Password  string    `json:"password" bson:"password" validate:"omitempty,required,gte=6"`
 }
 
 func (user *User) Validate() error {
