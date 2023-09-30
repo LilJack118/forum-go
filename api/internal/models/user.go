c47fc29c555bd38c5a548db322a4b70a41bbc2ed
Liljack118 <kamilzajaczek4@gmail.com>
2023-09-30 20:17:02 +0200

    add hash and check hash password functions

--- api/internal/models/user.go
+++ api/internal/models/user.go
@@ -2,14 +2,16 @@ package models
 
 import (
 	"forum/api/pkg/validator"
+	"strings"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
 	ID        uuid.UUID `json:"user_id" db:"user_id" validate:"omitempty"`
-	FirstName string    `json:"first_name" db:"first_name" validate:"required,lte=30"`
-	LastName  string    `json:"last_name" db:"last_name" validate:"required,lte=30"`
+	FirstName string    `json:"first_name" db:"first_name" validate:"required,alphanum,lte=30"`
+	LastName  string    `json:"last_name" db:"last_name" validate:"required,alphanum,lte=30"`
 	Email     string    `json:"email" db:"email" validate:"omitempty,lte=60,email"`
 	Password  string    `json:"password" db:"password" validate:"omitempty,required,gte=6"`
 }
@@ -18,8 +20,35 @@ func (user *User) Validate() error {
 	return validator.ValidateStruct(user)
 }
 
+func (user *User) HashPassword() error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	user.Password = string(hashedPassword)
+	return nil
+}
+
+func (user *User) CheckPassword(password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (user *User) PrepareCreate() error {
 	user.ID = uuid.New()
+
+	user.FirstName = strings.TrimSpace(user.FirstName)
+	user.LastName = strings.TrimSpace(user.LastName)
+	user.Email = strings.ToLower(strings.TrimSpace(user.Email))
+
+	if err := user.HashPassword(); err != nil {
+		return err
+	}
 
 	return nil
 }
