5053d0a78c3c37b1e2b56b346d569be622a6ec65
Liljack118 <kamilzajaczek4@gmail.com>
2023-10-14 13:29:45 +0200

    render posts list

--- frontend/src/components/feed/PostsList.vue
+++ frontend/src/components/feed/PostsList.vue
@@ -2,8 +2,11 @@
     Posts List
 
     <div>
-        <div v-for="post in posts">
-            {{ post }}
+        <div v-for="post in posts" class="post card" v-on:click="openPost(post.id)">
+            <div class="card-body text-start">
+                <h5 class="card-title fw-bold">{{ post.title }}</h5>
+                <p class="card-text">{{ post.content }}</p>
+            </div>
         </div>
     </div>
 </template>
@@ -23,16 +26,6 @@ export default {
     mounted() {
         this.loadPosts(this.page, this.limit);
     },
-    methods: {
-        async loadPosts(page, limit) {
-            let res = await axios.get(`api/posts?page=${page}&limit=${limit}`);
-            if (res.status != 200) {
-                console.log(res);
-            } else {
-                this.pageData = res.data;
-            }
-        }
-    },
     computed: {
         posts() {
             if (this.pageData.posts) {
@@ -41,8 +34,30 @@ export default {
                 return []
             }
         }
-    }
+    },
+    methods: {
+        async loadPosts(page, limit) {
+            let res = await axios.get(`api/posts?page=${page}&limit=${limit}`);
+            if (res.status != 200) {
+                console.log(res);
+            } else {
+                this.pageData = res.data;
+            }
+        },
+        openPost(id) {
+            this.$router.push({ name: 'post-page', params: { id: id } })
+        }
+    },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.post {
+    transition: all ease-in-out 0.2s;
+}
+
+.post:hover {
+    cursor: pointer;
+    transform: scale(1.01);
+}
+</style>
