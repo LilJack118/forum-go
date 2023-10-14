44630fc5dbfbd00570930724a0af796e6fe91e7a
Liljack118 <kamilzajaczek4@gmail.com>
2023-10-14 14:45:35 +0200

    add create post page

--- frontend/src/pages/CreatePostPage.vue
+++ frontend/src/pages/CreatePostPage.vue
@@ -1,11 +1,59 @@
 <template>
-    Create Post
+    <h1>Create Post</h1>
+    <hr>
+
+    <form>
+        <!-- Name input -->
+        <MDBInput type="text" label="title" id="title" v-model="post.title" wrapperClass="mb-4" />
+
+        <!-- Message input -->
+        <MDBTextarea label="Content" id="content" v-model="post.content" wrapperClass="mb-4" />
+        <!-- Submit button -->
+        <MDBBtn color="primary" block class="mb-4"> Create </MDBBtn>
+    </form>
 </template>
   
 <script>
 export default {
     name: 'CreatePostPage',
     components: {},
+    data() {
+        return {
+            post: reactive({
+                title: "",
+                content: "",
+            })
+        }
+    },
+    methods: {
+        createPost() {
+            axios.post('api/post', this.post)
+                .then(res => {
+                    this.handleAuthSuccess(res);
+                })
+                .catch(err => {
+                    this.handleRequestError(err);
+                    this.cleanPostData();
+                })
+        },
+        handleAuthSuccess(res) {
+            window.alert("Successfully created new post!")
+            this.cleanPostData();
+        },
+        handleRequestError(err) {
+            if (err.response.data.detail) {
+                let errorMsg = err.response.data.detail;
+            } else {
+                let errorMsg = "Try again later";
+            }
+
+            window.alert(errorMsg);
+        },
+        cleanPostData() {
+            this.post.title = '';
+            this.post.content = '';
+        },
+    }
 }
 
 </script>
