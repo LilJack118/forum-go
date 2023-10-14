4cbe852a7e93b39832be72c93c8d4ad4bacd11e8
Liljack118 <kamilzajaczek4@gmail.com>
2023-10-14 12:43:53 +0200

    add base feed page template

--- frontend/src/pages/FeedPage.vue
+++ frontend/src/pages/FeedPage.vue
@@ -1,11 +1,52 @@
 <template>
-    Feed Page
+    <h2>Feed</h2>
+
+    <hr>
+
+    <MDBTabs v-model="activeTab">
+        <!-- Tabs navs -->
+        <MDBTabNav tabsClasses="mb-3" class="justify-content-center">
+            <MDBTabItem tabId="all-posts" href="all-posts">All Posts</MDBTabItem>
+            <MDBTabItem tabId="my-posts" href="my-posts">My Posts</MDBTabItem>
+        </MDBTabNav>
+
+        <!-- Tabs content -->
+        <MDBTabContent class="mt-4">
+            <MDBTabPane tabId="all-posts">
+                All Posts
+            </MDBTabPane>
+
+            <MDBTabPane tabId="my-posts">
+                My Posts
+            </MDBTabPane>
+        </MDBTabContent>
+    </MDBTabs>
 </template>
   
 <script>
+import {
+    MDBTabs,
+    MDBTabNav,
+    MDBTabContent,
+    MDBTabItem,
+    MDBTabPane,
+} from 'mdb-vue-ui-kit';
+import { ref } from 'vue';
+
 export default {
     name: 'FeedPage',
-    components: {},
+    components: {
+        MDBTabs,
+        MDBTabNav,
+        MDBTabContent,
+        MDBTabItem,
+        MDBTabPane,
+    },
+    data() {
+        return {
+            activeTab: ref('all-posts')
+        }
+    }
 }
 
 </script>
